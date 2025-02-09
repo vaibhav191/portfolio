c4521c50d09d725b5996c9ef5cf4b615bd044d20
root <root@VaibhavEnvy>
2025-02-08 22:23:50 -0700

    fix: vite config for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/', // Add this line - should match your repo name
 })
